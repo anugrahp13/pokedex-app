e469a476130c79ce515d77cc19700986ec72db3a
anugrahp13 <anugrahprastyo13@gmail.com>
2026-06-03 14:42:50 +0700

    feat: register DetailPage route /pokemon/:name in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import HomePage from './pages/HomePage';
+import HomePage   from './pages/HomePage';
+import DetailPage from './pages/DetailPage';
 
 const queryClient = new QueryClient();
 
@@ -9,8 +10,8 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          {/* DetailPage akan ditambah di commit berikutnya */}
+          <Route path="/"                element={<HomePage />}   />
+          <Route path="/pokemon/:name"   element={<DetailPage />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
